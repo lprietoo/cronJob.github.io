304f1a6fe51b226882d380694f192bbd8b63346a
Laura Prieto <lprieto@gitei.edu.co>
2021-06-22 04:24:57 -0500

    added advances

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const cron = require("node-cron");
-const { db, http, signs } = require("./util");
 
-class Main {
-  static async getHoroscope() {
-    const todayHoroscope = [];
-    for (const sign of signs) {
-      const { horoscope } = (await http.get("/" + sign)).data;
-      todayHoroscope.push({
-        sign,
-        horoscope
-      });
-    }
 
-    const name = new Date()
-      .toDateString()
-      .split(" ")
-      .join("_");
 
-    db.setItem(name, JSON.stringify(todayHoroscope));
-  }
-}
 
 cron.schedule("* * * * *", () => {
   console.log('hi')
